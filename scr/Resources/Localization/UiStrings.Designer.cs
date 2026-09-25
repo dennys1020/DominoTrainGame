496f494af82fee1dfd1e1d10c292e8e49d69a149
dennys1020 <denisseyamileth57@gmail.com>
2026-09-24 21:40:01 -0600

    Created the UI friendsList and VerificationCode

--- scr/Resources/Localization/UiStrings.Designer.cs
+++ scr/Resources/Localization/UiStrings.Designer.cs
@@ -97,11 +97,65 @@ namespace DominoTrainGame.Resources.Localization {
         }
         
         /// <summary>
+        ///   Busca una cadena traducida similar a Volver a inicio de sesión.
+        /// </summary>
+        public static string BackToLoginLabel {
+            get {
+                return ResourceManager.GetString("BackToLoginLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Aceptar.
+        /// </summary>
+        public static string ButtonAccept {
+            get {
+                return ResourceManager.GetString("ButtonAccept", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Agregar.
+        /// </summary>
+        public static string ButtonAdd {
+            get {
+                return ResourceManager.GetString("ButtonAdd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Rechazar.
+        /// </summary>
+        public static string ButtonReject {
+            get {
+                return ResourceManager.GetString("ButtonReject", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Busca una cadena traducida similar a Eliminar.
         /// </summary>
         public static string ButtonRemove {
             get {
                 return ResourceManager.GetString("ButtonRemove", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Reenviar código.
+        /// </summary>
+        public static string ButtonResendCode {
+            get {
+                return ResourceManager.GetString("ButtonResendCode", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a  Verificar.
+        /// </summary>
+        public static string ButtonVerify {
+            get {
+                return ResourceManager.GetString("ButtonVerify", resourceCulture);
             }
         }
         
@@ -547,6 +601,15 @@ namespace DominoTrainGame.Resources.Localization {
         }
         
         /// <summary>
+        ///   Busca una cadena traducida similar a AMIGOS.
+        /// </summary>
+        public static string OpenFriendsListLabel {
+            get {
+                return ResourceManager.GetString("OpenFriendsListLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Busca una cadena traducida similar a PERFIL.
         /// </summary>
         public static string OpenProfileButtonLabel {
@@ -745,6 +808,24 @@ namespace DominoTrainGame.Resources.Localization {
         }
         
         /// <summary>
+        ///   Busca una cadena traducida similar a Amigos.
+        /// </summary>
+        public static string TabFriends {
+            get {
+                return ResourceManager.GetString("TabFriends", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Solicitudes.
+        /// </summary>
+        public static string TabRequests {
+            get {
+                return ResourceManager.GetString("TabRequests", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Busca una cadena traducida similar a ACTUALIZAR.
         /// </summary>
         public static string UpdatePasswordButtonLabel {
@@ -768,6 +849,51 @@ namespace DominoTrainGame.Resources.Localization {
         public static string UsernameLabel {
             get {
                 return ResourceManager.GetString("UsernameLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Código de verificación.
+        /// </summary>
+        public static string VerificationCodeAccessibleName {
+            get {
+                return ResourceManager.GetString("VerificationCodeAccessibleName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a ¿No recibiste el correo?.
+        /// </summary>
+        public static string VerificationNoEmail {
+            get {
+                return ResourceManager.GetString("VerificationNoEmail", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Ingresa el código de 5 dígitos que enviamos a tu correo.
+        /// </summary>
+        public static string VerificationPrompt {
+            get {
+                return ResourceManager.GetString("VerificationPrompt", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Ingrese el código.
+        /// </summary>
+        public static string VerificationTittle {
+            get {
+                return ResourceManager.GetString("VerificationTittle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Verificación.
+        /// </summary>
+        public static string VerificationWindowTitle {
+            get {
+                return ResourceManager.GetString("VerificationWindowTitle", resourceCulture);
             }
         }
         
