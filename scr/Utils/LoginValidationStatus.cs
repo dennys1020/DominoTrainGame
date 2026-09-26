f83f6f39927a7e61f4ed32713d7570c5552093b9
dennys1020 <denisseyamileth57@gmail.com>
2026-09-26 15:14:33 -0600

    Add GUIs "Recover password", "Learboard", loginValidator and login status

---
+++ scr/Utils/LoginValidationStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace DominoTrainGame.Validator;
+
+/// <summary>
+/// Possible outcomes when validating a login attempt.
+/// The GUI layer (LogInWindow) maps each value to a localized message from UiStrings.
+/// </summary>
+public enum LoginValidationStatus
+{
+    Success,
+    EmptyFields,
+    UserNotFound,
+    IncorrectPassword,
+    DatabaseError
+}
---
+++ scr/Utils/LoginValidator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using DominoTrainGame.Resources.Localization;
+using log4net.Ext.Trace;
+
+namespace DominoTrainGame;
+
+public sealed class LoginValidator
+{
+    private static readonly ITraceLog _logger;
+
+    static LoginValidator()
+    {
+        _logger = TraceLogManager.GetLogger(typeof(LoginValidator));
+    }
+
+    public bool TryLogin(string usernameOrEmail, string password, out string message)
+    {
+        if (string.IsNullOrWhiteSpace(usernameOrEmail) || string.IsNullOrWhiteSpace(password))
+        {
+            message = UiStrings.MessageRequiredFields;
+            return false;
+        }
+
+        try
+        {
+            using (DominoTrainBDEntities dbContext = new DominoTrainBDEntities())
+            {
+                Players user = dbContext.Players.FirstOrDefault(
+                    p => p.Username == usernameOrEmail || p.Email == usernameOrEmail);
+
+                if (user == null)
+                {
+                    message = UiStrings.MessageUserNotFound;
+                    return false;
+                }
+
+                string enteredHash = PasswordHasher.Hash(password);
+
+                if (user.Password == enteredHash)
+                {
+                    message = UiStrings.MessageLoginSuccess;
+                    return true;
+                }
+
+                message = UiStrings.MessageIncorrectPassword;
+                return false;
+            }
+        }
+        catch (Exception exception)
+        {
+            _logger.Error(
+                "The login operation failed due to a database exception.",
+                exception);
+
+            message = UiStrings.DatabaseErrorMessage;
+            return false;
+        }
+    }
+}
